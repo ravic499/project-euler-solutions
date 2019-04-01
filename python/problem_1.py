e73b46723ab93aed276a7b8117bf029b4f4145d0
Ravichandran K <30371896+ravic499@users.noreply.github.com>
2019-04-01 09:16:16 +0530

    [Project-Euler] Addressed the review comments
    Used python 3 specific syntax
    Added problem statement
    
    Testing   : Yet to test
    Reviewers : diseworks, srinathmkce

--- python/problem_1.py
+++ python/problem_1.py
@@ -1,7 +1,25 @@
-def calculate(limit):
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
+def calculate(number):
+    """To calculate sum of all the multiples of 3 or 5 below 'n' number
+
+    Args:
+        number (int) : Actual number limit
+
+    Returns:
+        int : Sum of all the multiples of 3 or 5 below 'n' number
+    """
     result = 0
-    for i in xrange(limit):
+
+    for i in range(number):
         if i % 3 == 0 or i % 5 == 0:
             result += i
-    print result
-calculate(1000)
+
+    return result
+
+print(calculate(1000))
